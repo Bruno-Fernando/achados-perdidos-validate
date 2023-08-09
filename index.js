2dbd780e9977dba0706bae5fcf310edff8d8e315
Bruno Fernando <brunofernando.sv@gmail.com>
2023-08-09 17:23:22 -0300

    Express initial setup

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hello')
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  const { matricula, password } = req.body;
+  res.send(`Hello World! matricula: ${matricula}, password: ${password}`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
