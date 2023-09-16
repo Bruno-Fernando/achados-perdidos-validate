b8de6934371a7aafa1fc217e49b86199fa800e8d
Bruno Fernando <brunofernando.sv@gmail.com>
2023-09-16 17:57:10 -0300

    Remove puppeteer algorithm and use pdf-parse to verify user

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const puppeteer = require("puppeteer-extra");
-const StealthPlugin = require("puppeteer-extra-plugin-stealth");
-const RecaptchaPlugin = require("puppeteer-extra-plugin-recaptcha");
+const fileUpload = require("express-fileupload");
+const pdfParse = require("pdf-parse");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-const URL = "https://pre.ufcg.edu.br:8443/ControleAcademicoOnline/";
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(
@@ -15,71 +13,27 @@ app.use(
     origin: ["http://localhost:3000", "https://achados-perdidos.vercel.app"],
   })
 );
-puppeteer.use(StealthPlugin());
-puppeteer.use(
-  RecaptchaPlugin({
-    provider: {
-      id: "2captcha",
-      token: process.env.CAPTCHA_KEY,
-    },
-  })
-);
+app.use(fileUpload());
 
 app.post("/", async (req, res) => {
-  const { registrationCode, password } = req.body.data;
+  if (!req.files || !req.files.rdmfile) {
+    res.status(400);
 
-  // Inicia uma nova instancia do navegador
-  const browser = await puppeteer.launch({
-    headless: "new",
-    args: ["--no-sandbox"],
-  });
+    return res.json({ error: true, errorMsg: "No rdmfile provided" });
+  }
 
-  console.log("abriu o navegador");
+  try {
+    const parsedRdm = await pdfParse(req.files.rdmfile);
 
-  // Abre uma aba
-  const page = await browser.newPage();
-  console.log("abriu uma nova aba");
+    const isAuth = parsedRdm.text.includes(req.body.registrationCode);
 
-  // Vai para o controle academico
-  await page.goto(URL);
-  console.log("chegou no controle academico");
-
-  // Digita a matricula e a senha nos campos adequados
-  await page.type("#login", registrationCode);
-  await page.type("#senha", password);
-  console.log("digitou login e senha");
-
-  // Submete o form contendo matricula e senha e espera o retorno da api
-  await Promise.all([
-    page.click("button[type=submit]"),
-    page.solveRecaptchas(),
-    page.waitForNavigation({ waitUntil: "networkidle2" }),
-  ]);
-  console.log("fez o submit");
-
-  // Caso o login tenha acontecido com sucesso, procura na home do
-  // controle academico o número de matricula, caso esteja igual
-  // significa que o login aconteceu com sucesso
-  // caso contrario o login não foi efetuado
-  const isAuth = await page.evaluate((reqRegistrationCode) => {
-    const scrapedRegistrationCode = document
-      .querySelector(".col-sm-9 , .col-xs-7")
-      ?.innerHTML.split(" ")[0];
-
-    console.log("verificou se foi autenticado");
-    if (scrapedRegistrationCode === reqRegistrationCode) {
-      return true;
-    } else {
-      return false;
-    }
-  }, registrationCode);
-
-  // Fecha a instancia do navegador
-  await browser.close();
-
-  res.json({
-    isAuth,
-  });
+    return res.json({
+      isAuth,
+    });
+  } catch (error) {
+    res.status(400);
+    return res.json({ error: true, errorMsg: "Can't parse rdmfile" });
+  }
 });
 
 app.listen(PORT, () => {
